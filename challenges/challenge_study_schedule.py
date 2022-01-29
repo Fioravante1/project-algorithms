37f5bf8924c00b1626b8b7df12817b25948ebdae
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-01-29 18:27:31 -0300

    fix: lint trailing whitespace, over-indented

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,10 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    counter = 0 
+    counter = 0
     try:
         for period in permanence_period:
             if period[0] <= target_time <= period[1]:
                 counter += 1
     except TypeError:
-            return None
+        return None
 
     return counter
