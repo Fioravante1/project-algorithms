c47759d2d493d5b792a4b54c3df532cd135278e0
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-02-07 20:37:15 -0300

    feat: add funcao is anagram - requisito tres

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,16 @@
+def sort(word):
+    unsorted_list = list(word)
+    list_sorted = []
+
+    while unsorted_list:
+        small = min(unsorted_list)
+        list_sorted.append(small)
+        unsorted_list.pop(unsorted_list.index(small))
+
+    return "".join(list_sorted)
+
+
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui teste. """
+    if len(first_string) != len(second_string):
+        return False
+    return sort(first_string) == sort(second_string)
