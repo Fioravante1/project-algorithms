47243981c137fd08a3590851a72e79b3850688c2
Fioravante Chiozzi <f.ichiozzi@gmail.com>
2022-02-07 20:49:51 -0300

    feat: add funcao find duplicate - requisito quatro

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,2 +1,11 @@
 def find_duplicate(nums):
-    """ Faça o código aqui. """
+    list_number = list(set(nums))
+    if len(nums) <= 1:
+        return False
+
+    for number in list_number:
+        if type(number) == str or number < 0:
+            return False
+        if nums.count(number) > 1:
+            return number
+    return False
